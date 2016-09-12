c2b20dcbd13001f09f7001a99a09b675457de4ec
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2016-09-12 10:04:16 +0100

    Add US location

--- site/static/js/location.js
+++ site/static/js/location.js
@@ -1,6 +1,18 @@
 function init_map() {
-  var pos_us = {lat: 43.8985938, lng: -79.432076};
+  var pos_ca = {lat: 43.8985938, lng: -79.432076};
   var pos_gb = {lat: 51.498146, lng: -0.144880};
+  var pos_us = {lat: 34.388863, lng: -118.566141};
+
+  var map_ca = new google.maps.Map(document.getElementById('map_ca'), {
+    center: pos_ca,
+    scrollwheel: false,
+    zoom: 12
+  });
+  new google.maps.Marker({
+      map: map_ca,
+      position: pos_ca,
+      title: 'TimeCruncher CA'
+  });
 
   var map_us = new google.maps.Map(document.getElementById('map_us'), {
     center: pos_us,
