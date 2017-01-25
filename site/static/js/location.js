0903a4fccda1a138e291f5841c78b830d74675a6
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2017-01-25 17:21:29 +0000

    Change log and locs update

--- site/static/js/location.js
+++ site/static/js/location.js
@@ -1,38 +1,35 @@
+/* globals locations */
 function init_map() {
-  var pos_ca = {lat: 43.8985938, lng: -79.432076};
-  var pos_gb = {lat: 51.498146, lng: -0.144880};
-  var pos_us = {lat: 34.388863, lng: -118.566141};
+  var gb_location = get_company('gb');
+  var us_location = get_company('us');
+  create_map(gb_location);
+  create_map(us_location);
 
-  var map_ca = new google.maps.Map(document.getElementById('map_ca'), {
-    center: pos_ca,
-    scrollwheel: false,
-    zoom: 12
-  });
-  new google.maps.Marker({
-      map: map_ca,
-      position: pos_ca,
-      title: 'TimeCruncher CA'
-  });
+  function get_company(name) {
+    return $.grep(locations, function (loc) {
+      return loc.location_name == name;
+    })[0]
+  }
 
-  var map_us = new google.maps.Map(document.getElementById('map_us'), {
-    center: pos_us,
-    scrollwheel: false,
-    zoom: 12
-  });
-  new google.maps.Marker({
-      map: map_us,
-      position: pos_us,
-      title: 'TimeCruncher US'
-  });
-
-  var map_gb = new google.maps.Map(document.getElementById('map_gb'), {
-    center: pos_gb,
-    scrollwheel: false,
-    zoom: 12
-  });
-  new google.maps.Marker({
-      map: map_gb,
-      position: pos_gb,
-      title: 'TimeCruncher GB'
-  });
+  function create_map(locat) {
+    var _loc = {'lat': locat.lat, 'lng': locat.lng};
+    var map = new google.maps.Map(document.getElementById(locat.map_id), {
+      center: _loc,
+      scrollwheel: false,
+      zoom: 12
+    });
+    var infoWindow = new google.maps.InfoWindow({
+      content: '<div><strong>' + locat.name + '</strong><br>' + locat.address + '<br>' + locat.phone + '</div>'
+    });
+    var marker = new google.maps.Marker({
+      map: map,
+      position: _loc,
+      title: locat.title,
+      infoWindow: infoWindow
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.open(map, marker);
+    });
+    infoWindow.open(map, marker);
+  }
 }
